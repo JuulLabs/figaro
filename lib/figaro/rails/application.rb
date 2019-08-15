6766ddc21f6b7b7cf14b610a24efd7aae12ffcea
jmaggio <jacob.maggio@juul.com>
2019-08-15 11:45:37 -0400

    Only make default_environment public

--- lib/figaro/rails/application.rb
+++ lib/figaro/rails/application.rb
@@ -1,18 +1,19 @@
 module Figaro
   module Rails
     class Application < Figaro::Application
-      def default_path
-        rails_not_initialized! unless ::Rails.root
-
-        ::Rails.root.join("config", "application.yml")
-      end
-
       def default_environment
         unless ::ENV['STACK_NAME'].nil?
           "#{::Rails.env}/#{::ENV['STACK_NAME']}"
         else
           ::Rails.env
         end
+      end
+
+      private
+      def default_path
+        rails_not_initialized! unless ::Rails.root
+
+        ::Rails.root.join("config", "application.yml")
       end
 
       def rails_not_initialized!
